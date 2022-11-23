7455d5d519136eb8c314b42c32305166e648ec6e
daisuke.miyoshi <nana40481023@gmail.com>
2022-11-24 00:54:18 +0900

    Add signupとloginのエンドポイント追加

---
+++ controller/router.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func GetRouter() *gin.Engine {
+	router := gin.Default()
+
+	// corsの設定
+	setCors(router)
+
+	router.POST("/signup", postSingup)
+	router.POST("/login", postLogin)
+
+	return router
+}
+
+func setCors(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
