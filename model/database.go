4c93959a5b982450cc8b95851e54a838e802bd1f
daisuke.miyoshi <nana40481023@gmail.com>
2022-11-26 20:43:03 +0900

    Add データベースと接続

---
+++ model/database.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
+)
+
+var db *gorm.DB
+
+func init() {
+	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
+	dbconf := "user:password@tcp(db:3306)/lottery-map?charset=utf8mb4"
+	var err error
+	db, err = gorm.Open(mysql.Open(dbconf), &gorm.Config{})
+	if err != nil {
+		fmt.Println("データベース接続失敗")
+	} else {
+		fmt.Println("データベース接続成功")
+	}
+	
+	// dbをmigrateする
+	db.AutoMigrate(&User{})
+}
