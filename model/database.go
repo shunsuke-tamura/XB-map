8c0d711f87e7db12b7e5dff04024be5c5c60992c
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-04 00:41:15 +0900

    Add データベースのホスト名変更

--- model/database.go
+++ model/database.go
@@ -10,7 +10,7 @@ var db *gorm.DB
 
 func init() {
 	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	dbconf := "user:password@tcp(db:3306)/lottery-map?charset=utf8mb4"
+	dbconf := "user:password@tcp(db_xb:3306)/xb-map?charset=utf8mb4"
 	var err error
 	db, err = gorm.Open(mysql.Open(dbconf), &gorm.Config{})
 	if err != nil {
