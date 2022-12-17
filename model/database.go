05eb3b3e4c2579a86b9839fbc2346e18e17fdf39
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-18 00:39:35 +0900

    Add reviewテーブルをautomigrateするようにした

--- model/database.go
+++ model/database.go
@@ -10,7 +10,7 @@ var db *gorm.DB
 
 func init() {
 	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	dbconf := "user:password@tcp(db_xb:3306)/xb-map?charset=utf8mb4"
+	dbconf := "user:password@tcp(db_xb:3306)/xb-map?charset=utf8mb4&parseTime=true"
 	var err error
 	db, err = gorm.Open(mysql.Open(dbconf), &gorm.Config{})
 	if err != nil {
@@ -22,4 +22,5 @@ func init() {
 	// dbをmigrateする
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&Shop{})
+	db.AutoMigrate(&Review{})
 }
