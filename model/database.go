376f220a16f91fafda50a26e67331632c6a47957
daisuke.miyoshi <nana40481023@gmail.com>
2022-12-26 14:00:50 +0900

    Add データベース接続のコメントを追加

--- model/database.go
+++ model/database.go
@@ -9,7 +9,7 @@ import (
 var db *gorm.DB
 
 func init() {
-	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
+	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]&DATETIME型などの日時カラムを扱いたいので，trueを指定
 	dbconf := "user:password@tcp(db_xb:3306)/xb-map?charset=utf8mb4&parseTime=true"
 	var err error
 	db, err = gorm.Open(mysql.Open(dbconf), &gorm.Config{})
